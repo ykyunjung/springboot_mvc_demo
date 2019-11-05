5b200959d4fbb7df1a0a03e801f9e6219c74b9e0
jungihon <jungihon@JUNGIHON-KR.kr.oracle.com>
2019-11-05 14:11:18 +0900

    project upload

---
+++ src/main/java/demo/oci/mvc/controller/HelloController.java
@@ -0,0 +1,33 @@
+package demo.oci.mvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller 
+public class HelloController{
+
+   @RequestMapping("/")
+   public String index() {
+      return "index";
+   }
+
+   @PostMapping("/hello")
+   public String sayHello(@RequestParam("name") String name, Model model) throws Exception {
+
+      model.addAttribute("firstName", "test_first_name");
+      model.addAttribute("lastName", "test_last_name");
+      model.addAttribute("email", "email@oracle.com");
+      model.addAttribute("phone", "82-123-456-7890");
+      model.addAttribute("hireDate", "2019-11-05");
+      model.addAttribute("jobTitle", "Sales Consultant");
+
+      System.out.println(model.getAttribute("firstName"));
+      
+      return "hello";
+   }
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Oracle Cloud Infrastructure</title>
+</head>
+<body>
+  <h1>OCI Spring Boot Demo</h1>
+  <hr>
+
+  <h2>first name : ${firstName}</h2>
+  <h2>last name : ${lastName}</h2>
+  <h2>email : ${email}</h2>
+  <h2>phone number : ${phone}</h2>
+  <h2>hire date : ${hireDate}</h2>
+  <h2>job title : ${jobTitle}</h2>
+
+</body>
+</html>
