cc2bb639ad415d3164df9be0d5858832932c09f8
ykyunjung <33448250+ykyunjung@users.noreply.github.com>
2019-11-18 12:09:04 +0900

    Update HelloController.java

--- src/main/java/demo/oci/mvc/controller/HelloController.java
+++ src/main/java/demo/oci/mvc/controller/HelloController.java
@@ -1,5 +1,7 @@
 package demo.oci.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
